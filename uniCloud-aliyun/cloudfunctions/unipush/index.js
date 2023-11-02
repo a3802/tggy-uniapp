17f8e7ad6812c652c9a1fe98ec13aebd960ae72f
a3802 <253092329@qq.com>
2023-11-03 02:41:05 +0800

    iokiie

--- uniCloud-aliyun/cloudfunctions/unipush/index.js
+++ uniCloud-aliyun/cloudfunctions/unipush/index.js
@@ -1,14 +1,14 @@
 'use strict';
 const uniPush = uniCloud.getPushManager({appId:"__UNI__88811EE"})
 exports.main = async (event, context) => {
+	let obj = JSON.parse(event.body);  
     return await uniPush.sendMessage({  
         //填写上一步在uni-app客户端获取到的客户端推送标识push_clientid
-        "push_clientid": "自己的设备号",     
+        "push_clientid": obj.clientid,     
         "force_notification":true,  //填写true，客户端就会自动创建“通知栏消息”。  
-        "title": "您收到一条消息",      
-        "content": "请查看",  
-        "payload": {  
-            "text":"体验一下uni-push2.0"  
-        }  
+        "title": obj.title,      
+        "content": obj.content,  
+        "payload": obj.payload,
+		"request_id": obj.request_id
     }) 
 };
